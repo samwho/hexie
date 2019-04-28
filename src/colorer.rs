9ada94dea29c371c745e07df0c8b8e4ad4795a55
Sam <samwho@lbak.co.uk>
2019-04-28 14:33:50 +0100

    Tab spaces = 2, come at me.

--- src/colorer.rs
+++ src/colorer.rs
@@ -1,26 +1,26 @@
 use colored::*;
 
 pub trait Colorer {
-    fn color(&self, s: &str, prev: Option<u8>, cur: u8) -> String;
+  fn color(&self, writer: impl Write, s: &str, prev: Option<u8>, cur: u8) -> String;
 }
 
 pub struct AbsoluteColorer {}
 
 impl AbsoluteColorer {
-    pub fn new() -> Self {
-        AbsoluteColorer {}
-    }
+  pub fn new() -> Self {
+    AbsoluteColorer {}
+  }
 }
 
 impl Colorer for AbsoluteColorer {
-    fn color(&self, s: &str, _prev: Option<u8>, cur: u8) -> String {
-        match cur {
-            0 => s.white().dimmed(),     // null
-            36 => s.bright_green(),      // space
-            1...32 => s.cyan().dimmed(), // non-printable
-            33...126 => s.green(),       // printable
-            _ => s.yellow().dimmed(),
-        }
-        .to_string()
+  fn color(&self, s: &str, _prev: Option<u8>, cur: u8) -> String {
+    match cur {
+      0 => s.white().dimmed(),     // null
+      36 => s.bright_green(),      // space
+      1...32 => s.cyan().dimmed(), // non-printable
+      33...126 => s.green(),       // printable
+      _ => s.yellow().dimmed(),
     }
+    .to_string()
+  }
 }
--- src/range_reader.rs
+++ src/range_reader.rs
@@ -3,65 +3,65 @@ use std::io::{Read, Result};
 const BUF_SIZE: usize = 4096;
 
 pub struct RangeReader<R: Read> {
-    inner: R,
-    start: Option<usize>,
-    end: Option<usize>,
-    pos: usize,
+  inner: R,
+  start: Option<usize>,
+  end: Option<usize>,
+  pos: usize,
 }
 
 impl<R: Read> RangeReader<R> {
-    pub fn new(inner: R, start: Option<usize>, end: Option<usize>) -> Self {
-        RangeReader {
-            inner,
-            start,
-            end,
-            pos: 0,
+  pub fn new(inner: R, start: Option<usize>, end: Option<usize>) -> Self {
+    RangeReader {
+      inner,
+      start,
+      end,
+      pos: 0,
+    }
+  }
+
+  fn advance(&mut self, n: usize) -> Result<usize> {
+    let iterations = n / BUF_SIZE;
+    let tbuf: &mut [u8] = &mut [0; BUF_SIZE];
+    for _i in 0..iterations {
+      match self.inner.read_exact(tbuf) {
+        Ok(_) => {
+          self.pos += BUF_SIZE;
         }
+        Err(e) => return Err(e),
+      }
     }
 
-    fn advance(&mut self, n: usize) -> Result<usize> {
-        let iterations = n / BUF_SIZE;
-        let tbuf: &mut [u8] = &mut [0; BUF_SIZE];
-        for _i in 0..iterations {
-            match self.inner.read_exact(tbuf) {
-                Ok(_) => {
-                    self.pos += BUF_SIZE;
-                }
-                Err(e) => return Err(e),
-            }
-        }
+    let mut tvec = vec![0; n - self.pos];
+    match self.inner.read_exact(tvec.as_mut_slice()) {
+      Ok(_) => {
+        self.pos += tvec.len();
+      }
+      Err(e) => return Err(e),
+    };
 
-        let mut tvec = vec![0; n - self.pos];
-        match self.inner.read_exact(tvec.as_mut_slice()) {
-            Ok(_) => {
-                self.pos += tvec.len();
-            }
-            Err(e) => return Err(e),
-        };
-
-        Ok(self.pos)
-    }
+    Ok(self.pos)
+  }
 }
 
 impl<R: Read> Read for RangeReader<R> {
-    fn read(&mut self, buf: &mut [u8]) -> Result<usize> {
-        if self.end != None && self.pos >= self.end.unwrap() {
-            return Ok(0);
-        }
-
-        if self.pos == 0 && self.start != None {
-            self.advance(self.start.unwrap())?;
-        }
-
-        let n = self.inner.read(buf)?;
-        if let Some(end) = self.end {
-            if self.pos + n > end {
-                let r = end - self.pos;
-                self.pos += n;
-                return Ok(r);
-            }
-        }
-
-        Ok(n)
+  fn read(&mut self, buf: &mut [u8]) -> Result<usize> {
+    if self.end != None && self.pos >= self.end.unwrap() {
+      return Ok(0);
     }
+
+    if self.pos == 0 && self.start != None {
+      self.advance(self.start.unwrap())?;
+    }
+
+    let n = self.inner.read(buf)?;
+    if let Some(end) = self.end {
+      if self.pos + n > end {
+        let r = end - self.pos;
+        self.pos += n;
+        return Ok(r);
+      }
+    }
+
+    Ok(n)
+  }
 }
